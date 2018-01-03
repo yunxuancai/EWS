e261bfc6806622b5c1695e921ef57466d09e3f61
Cui Jiawei <892001108@qq.com>
2018-01-03 09:55:36 +0800

    bgm循环播放，等待老党修改clickCount

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ App({
     },
     onShow: function () {
         console.log('App Show')
+        wx.playBackgroundAudio({
+          dataUrl: 'http://ac-f5oadpis.clouddn.com/bf9103a7fea8dda196ab.mp3',
+          title: '一个人也要好好吃饭哦',
+          success: function (res) {
+            console.log('success');
+            console.log(res);
+          },
+          fail: function (res) {
+            console.log('fail');
+            console.log(res);
+          }
+        })
+        wx.onBackgroundAudioStop(function(){
+          console.log('stop');
+          wx.playBackgroundAudio({
+            dataUrl: 'http://ac-f5oadpis.clouddn.com/bf9103a7fea8dda196ab.mp3',
+            title: '一个人也要好好吃饭哦',
+            success: function (res) {
+              console.log('success');
+              console.log(res);
+            },
+            fail: function (res) {
+              console.log('fail');
+              console.log(res);
+            }
+          })
+        })
     },
     onHide: function () {
         console.log('App Hide')
